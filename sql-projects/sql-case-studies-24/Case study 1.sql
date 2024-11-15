f2c62d6197cd005c99baf3f70e5be69940c92a07
Gift Chinwendu Eze <74875008+wendiiking@users.noreply.github.com>
2024-11-15 11:52:48 +0100

    Update Case study 1.sql

--- sql-projects/sql-case-studies-24/Case study 1.sql
+++ sql-projects/sql-case-studies-24/Case study 1.sql
@@ -7,6 +7,15 @@ Address varchar(20));
 Alter table Value_customer
 Add Date_birth varchar(20);
 
+insert into Value_customer values
+(2001,'Seun',09079864378,'2 Ogun Street','23 Sept'),
+(2002,'Obi',07086655479,'13 Moore Road','11 Aug'),
+(2003,'Nneka',09003337899,'15 Lekki Avenue','3 Jan'),
+(2004,'Favour',08082289009,'2 Terra Street','30 Jun');
+
+select * from Value_customer;
+
+
 Create table Value_ledger
 (Sales_ID int primary key,
 Order_date varchar(15),
@@ -17,15 +26,6 @@ Total_amount varchar(15));
 
 Alter table Value_ledger
 Add Customer_ID int;
-
-insert into Value_customer values
-(2001,'Seun',09079864378,'2 Ogun Street','23 Sept'),
-(2002,'Obi',07086655479,'13 Moore Road','11 Aug'),
-(2003,'Nneka',09003337899,'15 Lekki Avenue','3 Jan'),
-(2004,'Favour',08082289009,'2 Terra Street','30 Jun');
-
-select * from Value_customer;
-
 
 insert into Value_ledger values
 (3151,'16 Aug','buckets',2,50000,10000,2004),
